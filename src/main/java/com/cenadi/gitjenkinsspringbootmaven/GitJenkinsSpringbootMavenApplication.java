7a94eb111ed0e2dc487b67db63d0f99c322d6b88
unknown <moussahousseini@gmail.com>
2023-03-02 13:19:29 -0800

    first commit

---
+++ src/main/java/com/cenadi/gitjenkinsspringbootmaven/GitJenkinsSpringbootMavenApplication.java
@@ -0,0 +1,13 @@
+package com.cenadi.gitjenkinsspringbootmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitJenkinsSpringbootMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitJenkinsSpringbootMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cenadi/gitjenkinsspringbootmaven/GitJenkinsSpringbootMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenadi.gitjenkinsspringbootmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitJenkinsSpringbootMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
